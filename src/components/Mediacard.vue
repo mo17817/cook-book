66cd507acf219b52c95cce2e340701d2b4f9b14e
mo17817 <lazerharry@gmail.com>
2022-02-15 22:28:50 -0500

    up to date

---
+++ src/components/Mediacard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="~assets/portland_streets2.jpeg" />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        This is a quick cooking book instruction on how to make beef franks with
+        toast
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+<script></script>
+<style></style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+    <div class="row" id="title">
+      <Mediacard> </Mediacard>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-
+import Mediacard from "components/Mediacard";
 export default defineComponent({
   name: "PageIndex",
+  component: { Mediacard },
 });
 </script>
