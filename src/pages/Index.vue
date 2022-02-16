8251bdee922f0f606ce194ec8a18f3d896f3bf06
mo17817 <lazerharry@gmail.com>
2022-02-16 16:49:54 -0500

    improved spacing and  added images

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,7 +15,7 @@
   </head>
   <q-page class="flex flex-center bg-accent">
     <!-- <div class="q-pa-lg row items-start"> -->
-    <div class="row q-pl-xl q-ml-xl items-start q-gutter-md">
+    <div class="row q-pt-xl q-pb-xl q-pl-xl q-ml-xl q-gutter-md" id="container">
       <q-card
         v-for="cards in recipeCards"
         :key="cards"
@@ -23,11 +23,15 @@
         align="center"
         id="cards"
       >
-        <img :src="cards.img" />
+        <!-- <q-img id="img" :src="cards.img" fit="none" /> -->
 
         <q-card-section>
-          <div class="text-h6">{{ cards.title }}</div>
-          <div class="text-subtitle2">{{ cards.cooktime }}</div>
+          <q-img id="img" :src="cards.img" fit="contain" />
+
+          <div class="text-h6" id="card-title">{{ cards.title }}</div>
+          <div class="text-subtitle2" id="card-cooktime">
+            {{ cards.cooktime }}
+          </div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
@@ -52,44 +56,50 @@ export default defineComponent({
           cooktime: "15min",
           description:
             "An easy to make meal that taste great and looks great too!",
-          img: [require("../assets/portland_streets2.jpeg")],
+          img: [require("../assets/fajita-4.jpg")],
           class: "my-card col-4",
+          id: "card-1",
         },
         {
           title: "Gourmet Ramen Noodles",
           cooktime: "20mins",
           description: "Taking a classic and making it gourmet",
-          img: [require("../assets/portland_streets2.jpeg")],
-          class: "my-card col-3 bg-info text-white",
+          img: [require("../assets/ramen.jpg")],
+          class: "my-card col-4 bg-info text-white",
+          id: "card-2",
         },
         {
           title: "Beans and Hotdogs",
           cooktime: "20mins",
           description: "Beans + Hotdogs, need I say more?",
-          img: [require("../assets/portland_streets2.jpeg")],
-          class: "my-card col-4",
+          img: [require("../assets/hotdogs.jpg")],
+          class: "my-card col-2",
+          id: "card-3",
         },
         {
           title: "Beef Quesodillas",
           cooktime: "15mins",
           description: "A tasty and easy to make dish for kids and adults!",
-          img: [require("../assets/portland_streets2.jpeg")],
+          img: [require("../assets/quesodilla.jpg")],
           class: "my-card col-4",
+          id: "card-4",
         },
         {
           title: "Traditional Zalabia",
           cooktime: "30mins",
           description:
             "A traditional African deseart to make your mouth water and your stomach grumble",
-          img: [require("../assets/portland_streets2.jpeg")],
-          class: "my-card col-3 bg-info text-white",
+          img: [require("../assets/zalabia.jpg")],
+          class: "my-card col-4 bg-info text-white",
+          id: "card-5",
         },
         {
           title: "Ethopian Chia",
           cooktime: "45mins",
           description: "Taking a classic Ethopian drink and spazzing it up",
-          img: [require("../assets/portland_streets2.jpeg")],
-          class: "my-card col-4",
+          img: [require("../assets/tea.jpg")],
+          class: "my-card col-2",
+          id: "card-6",
         },
       ],
     };
@@ -100,5 +110,35 @@ export default defineComponent({
 #cards {
   font-family: "Exo 2", sans-serif;
   font-weight: 300;
+  /* inset-inline: left; */
+  /* justify-content: inherit; */
+  /* white-space: pre-wrap; */
+  /* inset-inline-start: 200; */
+}
+#container {
+  padding-left: 10%;
+}
+#img {
+  object-fit: cover;
+}
+@media screen and (max-width: 300px) {
+  #cards {
+    font-size: small;
+    max-width: 300px;
+    flex-wrap: wrap;
+    object-fit: contain;
+    height: 200px;
+    -ms-flex-direction: column-reverse;
+    flex-direction: column-reverse;
+  }
+  #img {
+    object-fit: fill;
+  }
+  #card-cooktime {
+    font-size: small;
+  }
+  #card-title {
+    font-size: small;
+  }
 }
 </style>
